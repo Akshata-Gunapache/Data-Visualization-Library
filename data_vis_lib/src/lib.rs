3b386b3374384bb03f628c36273b982d93c8ba8c
Akshata Gunapache <agunapache28@gmail.com>
2024-06-24 20:37:10 +0530

    changes to lib.rs

--- data_vis_lib/src/lib.rs
+++ data_vis_lib/src/lib.rs
@@ -1,38 +1,14 @@
-use wasm_bindgen::prelude::*;
-use serde::{Serialize, Deserialize};
-
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-pub struct ChartData {
-    data: Vec<f64>,
-    labels: Vec<String>,
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
 }
 
-#[wasm_bindgen]
-impl ChartData {
-    #[wasm_bindgen(constructor)]
-    pub fn new(data: Vec<f64>, labels: Vec<String>) -> ChartData {
-        ChartData { data, labels }
-    }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    pub fn get_data(&self) -> JsValue {
-        JsValue::from_serde(&self.data).unwrap()
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
     }
-
-    pub fn get_labels(&self) -> JsValue {
-        JsValue::from_serde(&self.labels).unwrap()
-    }
-}
-
-#[wasm_bindgen]
-pub fn generate_bar_chart(data: &ChartData) -> String {
-    let svg = format!(
-        r#"<svg width="400" height="200" xmlns="http://www.w3.org/2000/svg">
-           <!-- SVG content here using data and labels -->
-           </svg>"#
-    );
-    svg
 }
